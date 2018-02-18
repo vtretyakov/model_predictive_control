2705d805cf3d63d7350e4d4bb866221a4043d603
vtretyakov <vtretyakov@synapticon.com>
2018-02-18 12:29:44 +0100

    introduce weights to cost functions

--- src/MPC.cpp
+++ src/MPC.cpp
@@ -54,23 +54,33 @@ class FG_eval {
     // Any additions to the cost should be added to `fg[0]`.
     fg[0] = 0;
     
+    //cost fuctions weights
+    int cte_weight = 1;
+    int epsi_weight = 1;
+    int v_weight = 1;
+    int delta_weight = 1;
+    int a_weight = 1;
+    int delta_rate_weight = 500;
+    int a_rate_weight = 1;
+    
+    
     // The part of the cost based on the reference state.
     for (int t = 0; t < N; t++) {
-      fg[0] += CppAD::pow(vars[cte_start + t], 2);
-      fg[0] += CppAD::pow(vars[epsi_start + t], 2);
-      fg[0] += CppAD::pow(vars[v_start + t] - ref_v, 2);
+      fg[0] += cte_weight * CppAD::pow(vars[cte_start + t], 2);
+      fg[0] += epsi_weight * CppAD::pow(vars[epsi_start + t], 2);
+      fg[0] += v_weight * CppAD::pow(vars[v_start + t] - ref_v, 2);
     }
     
     // Minimize the use of actuators.
     for (int t = 0; t < N - 1; t++) {
-      fg[0] += CppAD::pow(vars[delta_start + t], 2);
-      fg[0] += CppAD::pow(vars[a_start + t], 2);
+      fg[0] += delta_weight * CppAD::pow(vars[delta_start + t], 2);
+      fg[0] += a_weight * CppAD::pow(vars[a_start + t], 2);
     }
     
     // Minimize the value gap between sequential actuations.
     for (int t = 0; t < N - 2; t++) {
-      fg[0] += 500*CppAD::pow(vars[delta_start + t + 1] - vars[delta_start + t], 2);//500 here is from the quiz, to be tuned
-      fg[0] += CppAD::pow(vars[a_start + t + 1] - vars[a_start + t], 2);
+      fg[0] += delta_rate_weight * CppAD::pow(vars[delta_start + t + 1] - vars[delta_start + t], 2);
+      fg[0] += a_rate_weight * CppAD::pow(vars[a_start + t + 1] - vars[a_start + t], 2);
     }
     
     //
