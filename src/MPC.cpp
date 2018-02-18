8868d2758a526799c0db00390c899c86e130e2b3
vtretyakov <vtretyakov@synapticon.com>
2018-02-18 12:01:49 +0100

    increase ipopt solver timout
    needs to be reduced to default 0.5 before the submission

--- src/MPC.cpp
+++ src/MPC.cpp
@@ -243,7 +243,7 @@ vector<double> MPC::Solve(Eigen::VectorXd state, Eigen::VectorXd coeffs) {
   options += "Sparse  true        reverse\n";
   // NOTE: Currently the solver has a maximum time limit of 0.5 seconds.
   // Change this as you see fit.
-  options += "Numeric max_cpu_time          0.5\n";
+  options += "Numeric max_cpu_time          3.5\n";
 
   // place to return solution
   CppAD::ipopt::solve_result<Dvector> solution;
