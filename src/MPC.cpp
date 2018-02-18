88b8dda35a90c2d83cc3f204f517a94b9a7a519f
vtretyakov <vtretyakov@synapticon.com>
2018-02-18 14:03:21 +0100

    fix polynomial

--- src/MPC.cpp
+++ src/MPC.cpp
@@ -121,8 +121,8 @@ class FG_eval {
       AD<double> delta0 = vars[delta_start + t - 1];
       AD<double> a0 = vars[a_start + t - 1];
       
-      AD<double> f0 = coeffs[0] + coeffs[1] * x0;
-      AD<double> psides0 = CppAD::atan(coeffs[1]);
+      AD<double> f0 = coeffs[0] + coeffs[1] * x0 + coeffs[2] * pow(x0, 2) + coeffs[3] * pow(x0, 3);
+      AD<double> psides0 = CppAD::atan(coeffs[1] + 2*coeffs[2]*x0 + 3*coeffs[3]*pow(x0,2));
       
       // Here's `x` to get you started.
       // The idea here is to constraint this value to be 0.
